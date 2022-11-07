0feba2695d882cec5cfd660640862ec1ca0eda5a
eunji <hej0102@naver.com>
2022-11-07 19:45:13 +0900

    LaunchScreen

---
+++ ServiceLevelProject/ServiceLevelProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ServiceLevelProject
+//
+//  Created by 황은지 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
